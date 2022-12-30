d59f81230b3d42823d855d4f2d360ebb3e662609
Daniil Kirillov <chunaixxx@gmail.com>
2022-12-31 02:41:07 +0300

    Добавлены алиасы

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
+const path = require('path')
+
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ]
+	transpileDependencies: ['vuetify'],
+
+	configureWebpack: {
+		resolve: {
+			alias: {
+				'@components': path.resolve(__dirname, 'src/components'),
+				'@services': path.resolve(__dirname, 'src/services'),
+				'@api': path.resolve(__dirname, 'src/services/api'),
+			},
+		},
+	},
 }
