2fe6a43109f41f6ddb028077bac817b46ea06624
Daniil Kirillov <chunaixxx@gmail.com>
2022-12-31 02:47:26 +0300

    Добавлен алиас с утилитами

--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,7 @@ module.exports = {
 			alias: {
 				'@components': path.resolve(__dirname, 'src/components'),
 				'@services': path.resolve(__dirname, 'src/services'),
+				'@utils': path.resolve(__dirname, 'src/services/utils'),
 				'@api': path.resolve(__dirname, 'src/services/api'),
 			},
 		},
