feb2f902ae4c2f03792f820a7f5c2d3ded66f32f
Daniil Kirillov <chunaixxx@gmail.com>
2023-06-30 17:10:00 +0300

    Рефакторинг MSelect

--- src/components/common/MSelect.vue
+++ src/components/common/MSelect.vue
@@ -1,6 +1,6 @@
 <template>
 	<v-select
-		class="MapHeader__input MapHeader__input-faculty"
+		class="MSelect"
 		:menu-props="{ offsetY: false }"
 		:items="items"
 		v-model="_value"
@@ -21,36 +21,45 @@
 
 <script>
 export default {
+	name: 'MSelect',
+
 	props: {
 		value: {
 			type: [Object, String, Number, Boolean],
 			default: () => ({}),
 		},
+
 		label: {
 			type: String,
 			default: '',
 		},
+
 		noDataText: {
 			type: String,
 			default: '',
 		},
+
 		itemText: {
 			type: String,
 			default: null,
 		},
+
 		itemValue: {
 			type: String,
 			default: null,
 		},
+
 		items: {
 			type: Array,
 			default: () => [],
 		},
+
 		returnObject: {
 			type: Boolean,
 			default: true,
 		},
 	},
+
 	computed: {
 		_value: {
 			get() {
@@ -64,4 +73,6 @@ export default {
 }
 </script>
 
-<style></style>
+<style lang="sass">
+.MSelect
+</style>
