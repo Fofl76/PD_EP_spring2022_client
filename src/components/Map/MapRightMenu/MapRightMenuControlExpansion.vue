a40120d4ceac689ce926f62d0f59ae2804191700
Daniil Kirillov <chunaixxx@gmail.com>
2023-11-27 09:22:30 +0300

    Блок с с настройкой контроля вынесен в отдельный компонент и улучшен

---
+++ src/components/Map/MapRightMenu/MapRightMenuControlExpansion.vue
@@ -0,0 +1,61 @@
+<template>
+	<MapRightMenuExpansion class="MapRightMenuControlExpansion">
+		<template #header>
+			<div class="MapRightMenuControlExpansion__header">
+				<div>Настройки контроля</div>
+			</div>
+		</template>
+
+		<template>
+			<div class="MapRightMenuControlExpansion__content">
+				<v-radio-group
+					:value="currentControlTypeId"
+					@change="onChange"
+					hide-details="auto"
+				>
+					<v-radio
+						v-for="control in allControlTypes"
+						:key="control.id"
+						:label="`${control.name}`"
+						:value="control.id"
+					>
+						{{ control.name }}
+					</v-radio>
+				</v-radio-group>
+			</div>
+		</template>
+	</MapRightMenuExpansion>
+</template>
+
+<script>
+import MapRightMenuExpansion from './MapRightMenuExpansion.vue'
+
+export default {
+	name: 'MapRightMenuControlExpansion',
+	components: { MapRightMenuExpansion },
+
+	props: {
+		currentControlTypeId: {
+			type: Number,
+			default: null,
+		},
+
+		allControlTypes: {
+			type: Array,
+			default: () => [],
+		},
+	},
+
+	methods: {
+		onChange(value) {
+			this.$emit('changeControlType', value)
+		},
+	},
+}
+</script>
+
+<style lang="sass">
+.MapRightMenuControlExpansion
+    .v-input--selection-controls
+        margin-top: 0 !important
+</style>
